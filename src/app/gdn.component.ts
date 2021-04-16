c6b75ffa789c1540e4520b70bd3507560796a87c
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-04-16 09:36:20 +0200

    changes main component

---
+++ src/app/gdn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gdn-root',
+  template: '<h1>GeoDotNet events</h1>',
+  styles: ['']
+})
+export class GdnComponent {
+  title = 'geodotnet-events';
+}
