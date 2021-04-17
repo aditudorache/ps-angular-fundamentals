0d8762ac0829e1c1543814d2074a4b56af3d0328
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-04-17 09:21:54 +0200

    module 2

--- src/app/gdn.component.ts
+++ src/app/gdn.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gdn-root',
-  template: '<h1>GeoDotNet events</h1>',
+  template: `<h1>GeoDotNet events</h1>
+    <img src="/assets/images/basic-shield.png">
+  `,
   styles: ['']
 })
 export class GdnComponent {
