ec7e782bc86d3ca30e00ae85755919e8828f527a
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-04-18 07:35:46 +0200

    module 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,58 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ToastrService } from './common/toastr.service';
-import { Error404Component } from './errors/404.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import {
   EventsListComponent,
   EventThumbnailComponent,
+  EventService,
   EventDetailsComponent,
   CreateEventComponent,
   EventRouteActivator,
-  EventService,
-  EventListResolver
-} from './events/index';
-
-import { GdnAppComponent } from './gdn.component';
-import { NavBarComponent } from './nav/nav-bar.component';
-import { appRoutes } from './routes';
+  EventListResolver,
+  CreateSessionComponent
+} from './events/index'
+import { EventsAppComponent } from './events-app.component'
+import { NavBarComponent } from './nav/nav-bar.component'
+import { ToastrService } from './common/toastr.service'
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [
-    GdnAppComponent,
+    EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent,
     EventDetailsComponent,
-    Error404Component,
+    NavBarComponent,
     CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [
-    EventService,
-    ToastrService,
+    EventService, 
+    ToastrService, 
     EventRouteActivator,
-    {
-      provide: 'canDeactivateCreateEvent',
-      useValue: checkDirtyState,
-    },
-    EventListResolver
+    EventListResolver,
+    AuthService,
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }
   ],
-  bootstrap: [GdnAppComponent],
+  bootstrap: [EventsAppComponent]
 })
-
 export class AppModule {}
 
-
-function checkDirtyState(component:CreateEventComponent) {
+export function checkDirtyState(component:CreateEventComponent) {
   if (component.isDirty)
     return window.confirm('You have not saved this event, do you really want to cancel?')
   return true
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,21 @@
-import { Routes } from '@angular/router';
-import { EventsListComponent } from './events/events-list.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { Routes } from '@angular/router'
+import { EventsListComponent } from './events/events-list.component'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { CreateEventComponent } from './events/create-event.component'
-import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 import { EventListResolver } from './events/events-list-resolver.service'
 
 export const appRoutes: Routes = [
-  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
+  {
+    path: 'events/new',
+    component: CreateEventComponent,
+    canDeactivate: ['canDeactivateCreateEvent'],
+  },
   {
     path: 'events',
     component: EventsListComponent,
-    resolve: {resolvedData:  EventListResolver}
+    resolve: { events: EventListResolver },
   },
   {
     path: 'events/:id',
@@ -22,7 +26,6 @@ export const appRoutes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full' },
   {
     path: 'user',
-    loadChildren: () => import('./user/user.module')
-      .then(m => m.UserModule)
-  }
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
 ];
