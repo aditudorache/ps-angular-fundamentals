1145586bb3f1f9acd3f0fe11bd872d2cf9d07def
Adrian Tudorache <a.p.tudorache@gmail.com>
2021-04-18 11:28:25 +0200

    module 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import {
   EventsListComponent,
@@ -13,22 +13,24 @@ import {
   EventListResolver,
   CreateSessionComponent,
   SessionListComponent,
-  DurationPipe,
-} from './events/index';
-import { EventsAppComponent } from './events-app.component';
-import { NavBarComponent } from './nav/nav-bar.component';
-import { ToastrService } from './common/toastr.service';
-import { appRoutes } from './routes';
-import { Error404Component } from './errors/404.component';
-import { AuthService } from './user/auth.service';
+  DurationPipe
+} from './events/index'
+import { EventsAppComponent } from './events-app.component'
+import { NavBarComponent } from './nav/nav-bar.component'
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service'
+import { appRoutes } from './routes'
+import { Error404Component } from './errors/404.component'
+import { AuthService } from './user/auth.service'
 import { CollapsibleWellComponent } from './common/collapsible-well.component';
+
+declare let toastr:Toastr
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
@@ -41,27 +43,25 @@ import { CollapsibleWellComponent } from './common/collapsible-well.component';
     CreateSessionComponent,
     SessionListComponent,
     CollapsibleWellComponent,
-    DurationPipe,
+    DurationPipe
   ],
   providers: [
-    EventService,
-    ToastrService,
+    EventService, 
+    { provide: TOASTR_TOKEN, useValue: toastr}, 
     EventRouteActivator,
     EventListResolver,
     AuthService,
-    {
-      provide: 'canDeactivateCreateEvent',
-      useValue: checkDirtyState,
-    },
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }
   ],
-  bootstrap: [EventsAppComponent],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
 
-export function checkDirtyState(component: CreateEventComponent) {
+export function checkDirtyState(component:CreateEventComponent) {
   if (component.isDirty)
-    return window.confirm(
-      'You have not saved this event, do you really want to cancel?'
-    );
-  return true;
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
 }
